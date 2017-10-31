d1472ac6ffacf86443718f59906ff75ba0a7ad7b
yarynayavor <yourhurt09@gmail.com>
2017-10-31 22:33:19 +0200

    added main files with loading animation

---
+++ js/weather.js
@@ -0,0 +1,15 @@
+
+
+var xhr=new XMLHttpRequest();
+
+xhr.open("get","https://www.where-am-i.net");
+// https://api.darksky.net/forecast/e195f394d8c4e2cc9cd1e68eee4ed156/49.8407,24.0305");
+
+//var url="https://api.darksky.net/forecast/e195f394d8c4e2cc9cd1e68eee4ed156/49.8407,24.0305";
+//var r = createCORSRequest('GET', url);
+// r.send();
+
+// $.getJSON(r, function(forecast) {
+// 	console.log(forecast);
+// });
+
